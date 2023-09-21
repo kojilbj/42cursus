a560da6738e963752dc3bf44f75f5befa52ea38a
Koji Watanabe <kojwatan@c1r21s9.42tokyo.jp>
2023-09-21 17:06:33 +0900

    da

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -15,41 +15,48 @@ static size_t	ft_length(char *str)
 
 size_t	ft_strlcat(char *dest, const char *src, size_t dstsize)
 {
-	size_t	length;
-	int			i;
+	size_t	dest_len;
+	size_t	src_len;
+	size_t	space_left;
+	size_t	i;
 
+	dest_len = ft_length(dest);
+	src_len = ft_length((char *)src);
+	if (dstsize <= dest_len)
+		return (dstsize + src_len);
+	space_left = dstsize - dest_len - 1;
 	i = 0;
-	length = ft_length(dest);
-	if(dstsize == 0 || dstsize <= length)
-		return (dstsize += ft_length((char *)src));
-	while (src[i] && (int)dstsize - (int)length - 1 > i)
+	while (src[i] != '\0' && i < space_left)
 	{
-		dest[length + i] = src[i];
+		dest[dest_len + i] = src[i];
 		i++;
 	}
-	dest[length + i] = '\0';
-	return (length + i);
+	dest[dest_len + i] = '\0';
+	if (src_len > space_left)
+		return (dest_len + src_len);
+	else
+		return (dest_len + i);
 }
 
-#include<stdio.h>
-#include<string.h>
+// #include<stdio.h>
+// #include<string.h>
 
-int main() {
-    char dest[20] = "Hello, ";
-    const char *src1 = "World!";
-    const char *src2 = "OpenAI's GPT-3.5";
+// int main() {
+//     char dest[20] = "Hello, ";
+//     const char *src1 = "World!";
+//     const char *src2 = "OpenAI's GPT-3.5";
 
-    // テストケース 1: src1 を dest に追加
-    size_t result1 = ft_strlcat(dest, src1, sizeof(dest));
-    // size_t result1 = strlcat(dest, src1, sizeof(dest));
-    printf("Test Case 1: Result=%zu, Concatenated String=%s\n", result1, dest);
-    // 期待される出力: Result=13, Concatenated String="Hello, World!"
+//     // テストケース 1: src1 を dest に追加
+//     size_t result1 = ft_strlcat(dest, src1, sizeof(dest));
+//     // size_t result1 = strlcat(dest, src1, sizeof(dest));
+//     printf("Test Case 1: Result=%zu, Concatenated String=%s\n", result1, dest);
+//     // 期待される出力: Result=13, Concatenated String="Hello, World!"
 
-    // テストケース 2: src2 を dest に追加（サイズオーバー）
-    size_t result2 = ft_strlcat(dest, src2, sizeof(dest));
-    // size_t result2 = strlcat(dest, src2, sizeof(dest));
-    printf("Test Case 2: Result=%zu, Concatenated String=%s\n", result2, dest);
-    // 期待される出力: Result=24, Concatenated String="Hello, World!OpenAI's"
+//     // テストケース 2: src2 を dest に追加（サイズオーバー）
+//     size_t result2 = ft_strlcat(dest, src2, sizeof(dest));
+//     // size_t result2 = strlcat(dest, src2, sizeof(dest));
+//     printf("Test Case 2: Result=%zu, Concatenated String=%s\n", result2, dest);
+//     // 期待される出力: Result=24, Concatenated String="Hello, World!OpenAI's"
 
-    return 0;
-}
+//     return 0;
+// }
