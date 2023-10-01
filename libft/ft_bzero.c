1139d6bbc26d842932022dc7fd5a9bc34d04c0ec
Koji Watanabe <1mira.koji-roots.boreas.barkan@ezweb.ne.jp>
2023-10-01 14:19:23 +0900

    09.10.01

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -14,16 +14,5 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	size_t	i;
-	char	*str;
-
-	str = s;
-	i = 0;
-	if (n == 0)
-		return ;
-	while (i < n)
-	{
-		str[i] = 0;
-		i++;
-	}
+	ft_memset(s, 0, n);
 }
--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -15,18 +15,12 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	char	*dst;
-	size_t	i;
 
-	i = 0;
 	if (count > 0 && size > 0 && SIZE_MAX / count < size)
 		return (NULL);
 	dst = malloc(count * size);
 	if (dst == NULL)
 		return (NULL);
-	while (i < count * size)
-	{
-		dst[i] = 0;
-		i++;
-	}
+	ft_bzero((void *)dst, count * size);
 	return (dst);
 }
